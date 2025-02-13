6290df32e1ca18b5d05f0fec3065623d3ed3c857
Partzhh09 <parthchotaliya788@gmail.com>
2025-02-13 11:59:41 +0530

    added some files

--- C_lang.c
+++ C_lang.c
@@ -1,52 +1,52 @@
-// #include<stdio.h>
+#include<stdio.h>
 
-// int main()
-// {
-//     int arr [3] [3] = {1,2,3,4,5,6,7,8,9};
-//     printf("Print array sum: \n");
-
-//     int sum = 0;
-//     for (int i = 0; i < 3; i++)
-//     {
-//         for (int j = 0; j < 3; j++)
-//         {
-//             printf("%d \t", arr[i][j]);
-//             sum += arr[i][j];
-//         }
-//         printf("\n");
-//     }
-//     printf("Sum of Element: %d\n", sum);
-//     return 0;
-// }
-
-#include <stdio.h>
-void main()
+int main()
 {
-    int size;
-    printf("Enter the array of size :");
-    scanf("%d",&size);
+    int arr [3] [3] = {1,2,3,4,5,6,7,8,9};
+    printf("Print array sum: \n");
 
-    int arr[size];
-    printf("Enter the value of Array element : \n");
-
-    for (int i = 0; i < size; i++)
+    int sum = 0;
+    for (int i = 0; i < 3; i++)
     {
-        scanf("%d",&arr[i]);
-    }
-    int min = arr[0];
-    int max = arr[0];
-    for (int i = 1; i < size; i++)
-    {
-        if (arr[i] < min) {
-            min = arr[i];
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%d \t", arr[i][j]);
+            sum += arr[i][j];
         }
-        if (arr[i] > max) {
-            max = arr[i];
-        }
+        printf("\n");
     }
-    printf("Minimum value in the array: %d\n", min);
-    printf("Maximum value in the array: %d\n", max);
+    printf("Sum of Element: %d\n", sum);
+    return 0;
 }
+
+// #include <stdio.h>
+// void main()
+// {
+//     int size;
+//     printf("Enter the array of size :");
+//     scanf("%d",&size);
+
+//     int arr[size];
+//     printf("Enter the value of Array element : \n");
+
+//     for (int i = 0; i < size; i++)
+//     {
+//         scanf("%d",&arr[i]);
+//     }
+//     int min = arr[0];
+//     int max = arr[0];
+//     for (int i = 1; i < size; i++)
+//     {
+//         if (arr[i] < min) {
+//             min = arr[i];
+//         }
+//         if (arr[i] > max) {
+//             max = arr[i];
+//         }
+//     }
+//     printf("Minimum value in the array: %d\n", min);
+//     printf("Maximum value in the array: %d\n", max);
+// }
 
 // #include <stdio.h>
 // void main()
@@ -87,4 +87,25 @@ void main()
 //     }
     
 //     printf("Minimum value in the array: %d\n", min);
+// }
+
+// #include <stdio.h>
+
+// int main() {
+//     int arr[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+//     printf("Print array elements: \n");
+
+//     int sum = 0;
+
+//     for (int i = 0; i < 9; i++) {
+//         printf("%d \t", arr[i]);
+//         sum += arr[i];
+
+//         if ((i + 1) % 3 == 0) {
+//             printf("\n");
+//         }
+//     }
+
+//     printf("Sum of elements: %d\n", sum);
+//     return 0;
 // }
---
+++ HTML/DOM/script.js
@@ -0,0 +1,6 @@
+window.console.log("Hello, World!");
+
+console.log(window);
+
+window.document.body.style.backgroundColor = "gray";
+window.document.body.style.color = "white";
