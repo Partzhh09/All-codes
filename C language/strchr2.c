1c88a62a1476e136260f15fb445723563d750413
Partzhh09 <parthchotaliya788@gmail.com>
2025-02-14 14:29:10 +0530

    added some files

---
+++ C language/strchr2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char str[]="Hello World";
+    char *ptr = strchr(str,'W');
+    if (ptr)
+    {
+        printf("character found at index %d",ptr-str);
+    }
+    else
+    {
+        printf("character not found");
+    }
+    
+}
