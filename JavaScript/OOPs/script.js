e6f4841cf2bae5f24c5a2d55049357e7a8553a53
Partzhh09 <parthchotaliya788@gmail.com>
2025-03-17 10:10:16 +0530

    some file added

---
+++ JavaScript/OOPs/script.js
@@ -0,0 +1,10 @@
+/*OOPs in Javascript ->
+    Variable -> Data Member
+    Function -> Member Function / Method
+
+pillars of OOPs ->
+    encapsulation
+    Data Abstraction
+    Inheritance
+    Polymorphism
+*/
