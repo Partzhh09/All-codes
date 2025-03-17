0904bd1edf7f41fc4a5259e671b0225250cb2bdd
Partzhh09 <parthchotaliya788@gmail.com>
2025-03-17 14:01:24 +0530

    some files added

--- JavaScript/OOPs concept/script.js
+++ JavaScript/OOPs concept/script.js
@@ -8,3 +8,12 @@ pillars of OOPs ->
     Inheritance
     Polymorphism
 */
+class Car{
+    constructor(n,m){
+        this.name = n;
+        this.model = m;
+    }
+}
+const c1 = new Car('Audi','A8');
+const c2 = new Car('BMW','X5');
+console.log(c1,c2);
