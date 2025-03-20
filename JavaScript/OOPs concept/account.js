ed60787cdf7bf8d1dd83ef325b6306fab0edd74b
Partzhh09 <parthchotaliya788@gmail.com>
2025-03-20 11:53:46 +0530

    added some files

--- JavaScript/OOPs concept/account.js
+++ JavaScript/OOPs concept/account.js
@@ -28,24 +28,56 @@ class BankAccount{
         console.log("Amount Added Successfully");
     }
     checkbalance(){
-        console.log("Balance : ",this.balance);
+        console.log("Balance : ₹",this.balance);
     }
     userdetails(){
-        console.log("Name :",this.name,"| Age :",this.age);
+        console.log("-> Name :",this.name,"\n-> Age :",this.age);
     }
     loan(amount){
         if(amount > 50000){
             console.log("Loan Not Approved");
         }else{
+            this.balance += 100000;
             console.log("Loan Approved");
         }
     }
 }
 const b1 = new BankAccount('Parth',25,25000.00);
 
+b1.userdetails();
 b1.addamount(5000);
 b1.checkbalance();
 b1.withdraw(2000);
 b1.checkbalance();
-b1.userdetails();
 b1.loan(20000);
+b1.checkbalance();
+
+
+/*
+class car
+
+car name
+car model
+car engine type
+auto manual
+car price
+car color
+car company
+*/
+
+class Car{
+    constructor(n,m,e,a,p,c,co){
+        this.name = n;
+        this.model = m;
+        this.engine = e;
+        this.auto = a;
+        this.price = p;
+        this.color = c;
+        this.company = co;
+    }
+    printDetails(){
+        console.log("Car Name :",this.name,"\nCar Model :",this.model,"\nEngine Type :",this.engine,"\nAuto/Manual :",this.auto,"\nPrice : ₹",this.price,"\nColor :",this.color,"\nCompany :",this.company);
+    }
+}
+const c1 = new Car('Audi','A8','Petrol','Auto',8000000.00,'Black','Audi');
+c1.printDetails();
