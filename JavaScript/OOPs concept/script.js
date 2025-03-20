ee7325c74b2497d70c9ed1e47d5e621e21d3910a
Partzhh09 <parthchotaliya788@gmail.com>
2025-03-20 11:02:15 +0530

    added some files

--- JavaScript/OOPs concept/script.js
+++ JavaScript/OOPs concept/script.js
@@ -58,6 +58,7 @@ addamount()
 checkbalance()
 userdetails()
 loan()
+in small code
 */
 
 class BankAccount{
@@ -71,22 +72,22 @@ class BankAccount{
             console.log("Insufficient Balance");
         }else{
             this.balance -= amount;
-            console.log("Amount Withdrawn :",amount);
+            console.log("Amount Withdrawn Successfully");
         }
     }
     addamount(amount){
         this.balance += amount;
-        console.log("Amount Added :",amount);
+        console.log("Amount Added Successfully");
     }
     checkbalance(){
-        console.log("Current Balance :",this.balance);
+        console.log("Balance : ",this.balance);
     }
     userdetails(){
         console.log("Name :",this.name,"| Age :",this.age);
     }
     loan(amount){
         if(amount > 50000){
-            console.log("Loan not approved");
+            console.log("Loan Not Approved");
         }else{
             console.log("Loan Approved");
         }
@@ -94,8 +95,10 @@ class BankAccount{
 }
 const b1 = new BankAccount('Parth',25,25000.00);
 const b2 = new BankAccount('Manav',30,30000.00);
-b1.withdraw(20000);
+
 b1.addamount(5000);
 b1.checkbalance();
+b1.withdraw(2000);
+b1.checkbalance();
 b1.userdetails();
-b1.loan(40000);
+b1.loan(60000);
