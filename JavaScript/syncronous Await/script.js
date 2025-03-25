ed522c124ef4da549582ae3659cbcddc3f5fbf09
Partzhh09 <parthchotaliya788@gmail.com>
2025-03-25 09:58:03 +0530

    added some files

--- JavaScript/syncronous Await/script.js
+++ JavaScript/syncronous Await/script.js
@@ -5,3 +5,16 @@ console.log("2nd");
 setTimeout(() => {
     console.log("3rd");
 }, 3000);
+
+
+/*
+Single Threaded
+Multi Threaded
+synchronous
+Asynchronous
+Callback
+Promises
+Callback Hell
+Async Await
+*/
+
